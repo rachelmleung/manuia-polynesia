c2dd26449e71e47ae7580886c1e668d606b0d712
Rachel Leung <rachelmleung@gmail.com>
2019-01-06 17:37:02 -0800

    new styles tested with live theme

--- page-schedule.php
+++ page-schedule.php
@@ -18,17 +18,239 @@ get_header(); ?>
 		<div class="main-content" role="main">
 			<?php while ( have_posts() ) : the_post(); ?>
 				<?php the_content(); ?>  
-			<?php endwhile; // end of the loop. ?>
             
+<div class="class-intro"><p>All classes are subject to change – <a href="http://www.manuiapolynesia.com/contact-us/">Call or email</a> to confirm. <br>
+Monthly event calendars are available at the Daly City studio during class.</p>
+</div>
             
+<div class="class-schedule">
+<div class="class-day">
+<h2>Mondays</h2>
+<div class="each-class">
+<div class="class-time"><p>5:15pm – 6:00pm</p></div>
+<div class="class-name"><p>Beginner Girls</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Daly City Studio </a></div>
+<div class="class-info">
+
+<p>Interested in learning authentic Tahitian dance? We’ll start by teaching you the basic movements, and then put them into sequences to make a dance.</p>
+
+<p>This class is ideal for youth girls new or returning to dance.</p>
+
+ <p>No prior dance experience required.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+<div class="each-class">
+<div class="class-time"><p>6:15pm – 7:00pm</p></div>
+<div class="class-name"><p>Beginner Girls</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Daly City Studio</a></div>
+<div class="class-info">
+
+<p>Interested in learning authentic Tahitian dance? We’ll start by teaching you the basic movements, and then put them into sequences to make a dance.</p>
+
+<p>This class is ideal for youth girls new or returning to dance.</p>
+
+ <p>No prior dance experience required.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+<div class="each-class">
+<div class="class-time"><p>7:00pm – 8:30pm</p></div>
+<div class="class-name"><p>Beginner Adult Ladies</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Daly City Studio</a></div>
+<div class="class-info">
+
+<p>Explore the fundamentals of this unique dancing style that combines both live drumming and energetic choreography. Learn traditional moves like <span class="m_-5252755070066003672gmail-SetPageTerm-wordText"><span class="m_-5252755070066003672gmail-TermText m_-5252755070066003672gmail-notranslate m_-5252755070066003672gmail-lang-??">fa`arapu</span></span> (hip circles), <span class="m_-5252755070066003672gmail-SetPageTerm-wordText"><span class="m_-5252755070066003672gmail-TermText m_-5252755070066003672gmail-notranslate m_-5252755070066003672gmail-lang-??">tamau</span></span> (hip sway left &amp; right) and numerous variations. Get a great cardio workout while also toning up your abs, thighs and glutes!</p>
+
+<p>This class is ideal for adult ladies new or returning to dance.</p>
+
+ <p>No prior dance experience required.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+
+</div>
+<!-- end class day -->
+<div class="class-day">
+<h2>Tuesdays</h2>
+<div class="each-class">
+<div class="class-time"><p>6:00pm –  7:00pm</p></div>
+<div class="class-name"><p>Intermediate/Advanced Girls &amp; Boys</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Daly City Studio</a></div>
+<div class="class-info">
+
+<p>Go beyond the basics and learn more dance variations and techniques with our Intermediate and Advanced mixed girls and boys class. Continue your fundamental training while also expanding your stage performance skills and learning new dance routines.</p>
+
+<p>This class is ideal for youth girls and boys continuing dance.</p>
+
+ <p>Prior dance experience required. Please contact us for consideration.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+<div class="each-class">
+<div class="class-time"><p>7:00pm –  8:30pm</p></div>
+<div class="class-name"><p>Intermediate/Advanced Adult Ladies</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Daly City Studio</a></div>
+<div class="class-info">
+
+<p>Expand on your core moves with new variations and transitions for solo dancing and group dance routines. Intermediate and Advanced ladies get an intense ab and leg workout while also learning stage presence, choreography and how to perform in front of an audience.</p>
+
+<p>This class is ideal for adult ladies continuing dance.</p>
+
+ <p>Prior dance experience required. Please contact us for consideration.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+<div class="each-class">
+<div class="class-time"><p>8:15pm –  9:15pm</p></div>
+<div class="class-name"><p>Drills and Dance Breakdown</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Hayward Location</a></div>
+<div class="class-info">
+
+<p>Get a great cardio workout while practicing on your fundamental Tahitian dance skills including footwork, handwork and choreography. Learn routines and improvise dance sequences with different drum beats while toning up your abs, thighs and glutes!</p>
+
+<p>This class is ideal for newbies or someone returning to dance.</p>
+
+ <p>No prior dance experience required.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+
+</div>
+<!-- end class day -->
+<div class="class-day">
+<h2>Wednesdays</h2>
+<div class="each-class">
+<div class="class-time"><p>6:00pm –  7:00pm</p></div>
+<div class="class-name"><p>Advanced Girls &amp; Boys</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Daly City Studio</a></div>
+<div class="class-info">
+
+<p>You're ready to learn more advanced dance moves and learn how to perform in professional dance performances. Start to perfect your technique and timing, precision and execution of the dance moves while improving your flexibility, rhythm and balance.</p>
+
+<p>This class is ideal for youth girls and boys continuing Tahitian dance.</p>
+
+ <p>Prior dance experience required. Please contact us for consideration.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+<div class="each-class">
+<div class="class-time"><p>7:00pm –  8:30pm</p></div>
+<div class="class-name"><p>Advanced Adult Men &amp; Ladies</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Daly City Studio</a></div>
+<div class="class-info">
+
+<p>Apply your wide range of skills to learning dance routines for professional entertainment performances and dance competitions. Keep up with this fast-paced class by exhibiting proper basic and advanced steps, and outstanding technique, teamwork and showmanship.</p>
+
+<p>This class is ideal for advanced ladies and men continuing Tahitian dance.</p>
+
+ <p>Advanced experience required. Please contact us for consideration.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+
+</div>
+<!-- end class day -->
+<div class="class-day">
+<h2>Thursdays</h2>
+<div class="each-class">
+<div class="class-time"><p>7:00pm –  8:30pm</p></div>
+<div class="class-name"><p>Solo Competition training</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Daly City Studio</a></div>
+<div class="class-info">
+
+<p>Get professional instruction on how to choreograph solo dance routines, improve your stage performance and perfect your dance style. Learn to transition through various drum beat sequences and showcase your range of dance and performance skills to a panel of judges.</p>
+
+<p>This class is ideal for all ages and genders training for solo competition.</p>
+
+ <p>Beginner dance experience required.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+<div class="each-class">
+<div class="class-time"><p>8:15pm –  9:15pm</p></div>
+<div class="class-name"><p>Competition conditioning</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Daly City Studio</a></div>
+<div class="class-info">
+
+<p>Condition your legs for group performances or solo competition with this high intensity dance and strength training class. Practice exercises and drills specifically designed to build your leg muscles for endurance dancing.</p>
+
+<p>This class is ideal for all ages and genders training for competition.</p>
+
+ <p>Beginner dance experience required.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+<div class="each-class">
+<div class="class-time"><p>8:15pm –  9:15pm</p></div>
+<div class="class-name"><p>Drills and Dance Breakdown</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Hayward Location</a></div>
+<div class="class-info">
+
+<p>Get a great cardio workout while practicing on your fundamental Tahitian dance skills including footwork, handwork and choreography. Learn routines and improvise dance sequences with different drum beats while toning up your abs, thighs and glutes!</p>
+
+<p>This class is ideal for newbies or someone returning to dance.</p>
+
+ <p>No prior dance experience required.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+
+</div>
+<!-- end class day -->
+<div class="class-day">
+<h2>Fridays</h2>
+<div class="each-class">
+<div class="class-time"><p>6:00pm –  7:30pm</p></div>
+<div class="class-name"><p>Tahitian Drumming</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Daly City Studio</a></div>
+<div class="class-info">
+
+<p>Learn how to properly identify and play a variety of drum beats from Bora Bora to Samba while improving your rhythm and tempo. Practice with our award-winning drummers and musicians as they demonstrate the uses of different traditional Tahitian drums like the toere and the pahu.</p>
+
+<p>This class is ideal for all ages and genders interested in Tahitian drumming.</p>
+
+ <p>No prior drumming experience required.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+
+</div>
+<div class="class-day">
+<h2>Saturdays</h2>
+<div class="each-class">
+<div class="class-time"><p>10:00am – 11:30am</p></div>
+<div class="class-name"><p>Drills / Dance Breakdown / Solos</p></div>
+<div class="class-location"><a href="http://www.manuiapolynesia.com/locations/">Daly City Studio</a></div>
+<div class="class-info">
+
+<p>Join this all-levels class that provides an array of leg conditioning, dance breakdown and training for solo competition. Catch up on learning dance routines and drill with different exercises designed to perfect your basics and core training.</p>
+
+<p>This class is ideal for all ages and genders training for competition or preparing for performances.</p>
+
+ <p>Beginner dance experience required.</p> 
+
+</div>
+</div>
+<!-- end each class-->
+
+</div>
+</div>
             
+        <?php endwhile; // end of the loop. ?>      
 		</div><!-- .main-content -->
-        
-        
-        <div class="calendar-widget">
-        </div>
-        
-        <?php get_sidebar(); ?>
 
 	</div><!-- #primary -->
 
